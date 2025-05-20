51c434bc82369f3873b65300910b33365bcb633f
Fathan <102466798+mhmdfathan@users.noreply.github.com>
2025-05-20 14:47:46 +0700

    revert back to prisma

--- app/utils/db.ts
+++ app/utils/db.ts
@@ -4,14 +4,16 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
+if (!globalForPrisma.prisma) {
+  globalForPrisma.prisma = new PrismaClient({
     log:
       process.env.NODE_ENV === 'development'
         ? ['query', 'error', 'warn']
         : ['error'],
   });
+}
+
+const prisma = globalForPrisma.prisma;
 
 if (process.env.NODE_ENV !== 'production') {
   globalForPrisma.prisma = prisma;
